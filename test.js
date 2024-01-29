f60328dff52d7745408869d4ba9c6f7e4b65cf52
anoop-singh-layr <129151758+anoop-singh-layr@users.noreply.github.com>
2024-01-29 19:53:08 +0530

    Update and rename freeze.js to test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+ keys.forEach((key) => {
+    // Convert each key to PEM
+    const key_id = key.kid;
+    const modulus = key.n;
+    const exponent = key.e;
+    const key_type = key.kty;
+    const jwk = { kty: key_type, n: modulus, e: exponent };
+    const temp_pem = jwkToPem(jwk);
+    pems[key_id] = temp_pem;
+  });
