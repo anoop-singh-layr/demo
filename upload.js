805c7c68fa4ef07c871598f6a300c0f8d1dc4023
anoop-singh-layr <129151758+anoop-singh-layr@users.noreply.github.com>
2024-02-26 20:18:00 +0530

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,1 +1,14 @@
 
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const path = require('path');
+
+//configuring the AWS environment
+AWS.config.update({
+    accessKeyId: "<Access Key Here>",
+    secretAccessKey: "<Secret Access Key Here>"
+  });
+
+var s3 = new AWS.S3();
+var filePath = "./data/file.txt";
+
