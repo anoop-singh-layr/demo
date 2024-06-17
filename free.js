f068433f90be234caad74a5a37827312ca9be79a
anoop-singh-layr <129151758+anoop-singh-layr@users.noreply.github.com>
2024-06-17 19:36:28 +0530

    Update free.js

--- free.js
+++ free.js
@@ -17,7 +17,7 @@ export class ApiLambdaCrudDynamoDBStack extends Stack {
       tableName: 'items',
 
       /**
-       *  The default removal policy is RETAIN, which means that cdk destroy will not attempt to delete
+       * The default removal policy is RETAIN, which means that cdk destroy will not attempt to delete
        * the new table, and it will remain in your account until manually deleted. By setting the policy to
        * DESTROY, cdk destroy will delete the table (even if it has data in it)
        */
